d4520b6609bc13cb30eb0b7dd3d0ebf029b1a8f0
Kirill Donenko <kirill.donenko@gmail.com>
2017-10-11 23:41:03 +0300

    style owlcarousel

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,7 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    dots:false,
     responsive:{
         0:{
             items:1
@@ -13,6 +14,6 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     },
-    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>',
-    '<i class="fa fa-angle-right" aria-hidden="true"></i>']
+    navText : ['<i id="arrow-left" class="fa fa-angle-left" aria-hidden="true"></i>',
+    '<i id="arrow-right" class="fa fa-angle-right" aria-hidden="true"></i>']
 })
